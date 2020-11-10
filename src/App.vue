df8a14565cd487c1e04b6fd84e42f6158690f6ba
shshy81 <shshy81@gmail.com>
2020-11-10 14:35:30 +0900

    Todo List App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo List</h1>
+    <input 
+      type="text" class="w-100 p-2" placeholder="add Here"
+      @keyup.enter="addTodo"
+      v-model="todoText"
+    />
+    <hr>
+    <Todo 
+      v-for="todo in todos" :key="todo.id"
+      :todo="todo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from '@/components/Todo.vue';
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Todo
+  },
+  data() {
+    return {
+      todoText: '',
+      todos : [
+        {id:1, text: 'Buy a car', checked: false},
+        {id:2, text: 'play game', checked: false},
+      ]
+    }
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+      });
+      this.todoText = '';
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
